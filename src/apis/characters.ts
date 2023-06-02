718c7fbf6a6c5ae4622d75812315dc6eae6e6a90
Lee Joon ho <72702492+twoLoop-40@users.noreply.github.com>
2023-06-03 04:19:46 +0900

    getCharacter

--- src/apis/characters.ts
+++ src/apis/characters.ts
@@ -1,20 +1,45 @@
-async function makeResult(response?: Response) {
-  if (!response) {
-    return { ok: false };
+type CharacterOverAll = {
+  id: number;
+  name: string;
+  imageUrl: string;
+};
+
+type CharacterDetail = CharacterOverAll & {
+  films: string[];
+  sourceUrl: string;
+};
+
+type taggedCharacter<T> = T extends "Detail"
+  ? CharacterDetail & { type: T; ok: true }
+  : T extends "OverAll"
+  ? CharacterOverAll & { type: T; ok: true }
+  : { type: T; ok: false };
+
+async function getCharacter(
+  id?: number,
+  result?: CharacterOverAll | CharacterDetail
+) {
+  const apiAddress = "https://disney_api.nomadcoders.workers.dev/characters";
+  const fetcher = async (id?: number) => {
+    try {
+      const responseData = id
+        ? fetch(`${apiAddress}/${id}`)
+        : fetch(apiAddress);
+      const result = await responseData.then((response) => response.json());
+      return getCharacter(id, result);
+    } catch (err) {
+      console.log(err);
+      return { ok: false, type: "failure" } as taggedCharacter<"failure">;
+    }
+  };
+  if (result) {
+    return id
+      ? ({ ok: true, type: "Detail", result } as taggedCharacter<"Detail">)
+      : ({ ok: true, type: "OverAll", result } as teggedCharacter<"OverAll">);
   }
-  const result = await response.json();
-  return result;
-}
-async function getCharacters() {
-  try {
-    const response = await fetch(
-      "https://disney_api.nomadcoders.workers.dev/characters"
-    );
-    return makeResult(response);
-  } catch (err) {
-    console.log(err);
-    return makeResult();
-  }
+  fetcher(id);
 }
 
-export default getCharacters;
+const result = getCharacter(15, { id: 1, name: "이", imageUrl: "url" });
+
+export default getCharacter;

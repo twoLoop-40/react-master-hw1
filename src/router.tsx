4b12f2d3ca37a974a08c2ec40b9443570ccc9edd
Joon Ho Lee <jh.lee@spira-t.com>
2023-06-04 12:33:08 +0900

    App file move

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
+import App from "./pages/App";
 import AllCharacters from "./pages/allCharacters";
 import Detail from "./pages/detail";
 
